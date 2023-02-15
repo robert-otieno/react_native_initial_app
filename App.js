91077177288f2f16298810af0bc8d86cce95a832
Robert Otieno <robertotieno.or@gmail.com>
2023-02-15 14:31:26 +0300

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  }
 });
